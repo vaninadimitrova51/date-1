fc36f30035ba8e0dcc71345858f2a897f72a4c68
vaninadimitrova51 <vaninadimitrova51@abv.bg>
2018-02-25 15:01:39 +0200

    date+1

--- main.c
+++ main.c
@@ -1,109 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
 
     int day;
     int month;
     int year;
-    int newDay;
-    int numberOfDaysInMonth;
 
-    printf("Enter day\n");
+    printf("Enter the day\n");
     scanf("%d", &day);
 
-    printf("Enter month\n");
+    printf("Enter the month\n");
     scanf("%d", &month);
 
-    printf("Enter year\n");
+    printf("Enter the year\n");
     scanf("%d", &year);
 
-    printf("The date is %d/%d/%d\n\n", day, month, year);
+    printf("The date you entered is: %d/%d/%d\n", day, month, year);
 
-
-    if(year % 4 == 0){
-        printf("\nVisokosna\n(ne znam kak e na angliiski ne me sydi :D)\n\n");
-        if(month == 2){
-            if(day < 30){
-
-                void Months();
-
-                numberOfDaysInMonth = month;
-                newDay = day + 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-
-            }else if(day > 29){
-                printf("\nThis date does not exist\n");
-            }
-    }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-            if(day < 32){
-                numberOfDaysInMonth = month;
-                newDay = day + 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-            }else if(day > 31){
-                printf("\nThis date does not exist\n");
-            }
-        }else if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
-            if(day < 31){
-                numberOfDaysInMonth = month;
-                newDay = day - 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-            }else if(day > 30){
-                printf("\nThis date does not exist\n");
+    if(year < 0 || year > 9999 || month < 1 || month > 12 || day <= 0 || day > 31){
+        printf("Invalid date");
+    }else if(year % 4 == 0){
+        printf("Visokosna\n");
+        if(month == 2 && day > 29){
+            printf("Invalid date\n");
+        }else if(month == 4 || month == 6 || month == 9 || month == 11){
+            if(day > 30){
+                printf("Invalid date");
             }
         }
-
-
-
     }else if(year % 100 != 0){
-        printf("\nNe e visokosna\n(ne znam kak e na angliiski ne me sydi :D)\n");
-        if(month == 2){
-            if(day < 29){
-
-                numberOfDaysInMonth = month;
-                newDay = day - 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-
-            }else if(day > 28){
-            printf("\nThis date does not exist\n");
-            }
-        }else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-            if(day < 32){
-                numberOfDaysInMonth = month;
-                newDay = day + 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-            }else if(day > 31){
-                printf("\nThis date does not exist\n");
-            }
-        }else if(month == 2 || month == 4 || month == 6 || month == 9 || month == 11){
-            if(day < 31){
-                numberOfDaysInMonth = month;
-                newDay = day + 1;
-                    if(month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-                        day = 31;
-                    }
-                    else{
-                        day = 30;
-                    }
-                        month += 1;
-
-                printf("\nThe new date is: %d/%d/%d\n", newDay, numberOfDaysInMonth, year);
-            }else if(day > 30){
-                printf("\nThis date does not exist\n");
+        printf("Ne e visokosna\n");
+        if(month == 2 && day > 28){
+            printf("Invalid date\n");
+        }else if(month == 4 || month == 6 || month == 9 || month == 11){
+            if(day > 30){
+                printf("Invalid date");
             }
         }
-
-
     }
+
+    if(month != 3 && month != 1 && day == 1){
+        if(month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            day = 31;
+        }else{
+            day = 30;
+        }
+        month += 1;
+    }else if(month == 2){
+        if(year % 4){
+            day = 1;
+        }else if(year != 100){
+            day = 1;
+        }
+        month += 1;
+    }else if(month == 12 && day == 31){
+        day = 1;
+        month = 1;
+        year += 1;
+    }else{
+        day += 1;
+    }
+
+    printf("The new day is %d/%d/%d", day, month, year);
+
+
+
+
+
 
 
     return 0;
 }
-
